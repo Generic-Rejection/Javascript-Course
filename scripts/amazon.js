13467fe1101ee029afd9339ebbd047af44d00aea
mike <makogun.2204457@stu.cu.edu.ng>
2025-04-10 17:12:01 +0100

    It has an error, just keep for now

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,7 @@
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js'
+
+
 let productsHTML = '';
 
 products.forEach((products) => {
@@ -56,34 +60,22 @@ products.forEach((products) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-    button.addEventListener('click', () => {
-        const productId = button.dataset.productId;
-
-        let matchingItem;
-
-        cart.forEach((item) => {
-            if (productId === item.productId){
-                matchingItem = item;
-            }
-        });
-        if (matchingItem) {
-            matchingItem.quantity += 1
-        } else{
-            cart.push({
-                productId: productId,
-                quantity: 1
-            });
-        }
-
-        let cartQuantity = 0
+function updateCartQuantity() {
+  let cartQuantity = 0
 
 
-        cart.forEach((item) => {
-          cartQuantity += item.quantity ;
+        cart.forEach((cartItem) => {
+          cartQuantity += cartItem.quantity ;
         });
 
         document.querySelector('.js-cart-quantity')
           .innerHTML = cartQuantity;
+}
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
     });
 });
